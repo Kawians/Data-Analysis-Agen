07ad84a83a176029dbeab02870ad68900ddae1e2
Kavian <Kavian.Mashayekhi@Gmail.com>
2025-03-29 23:25:37 -0400

    Changing Model Name

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 # Configure Gemini
 import os
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel(model_name="models/gemini-pro")
+model = genai.GenerativeModel(model_name="models/gemini-2.0-flash")
 
 # Streamlit App
 st.title("🧠 AI Data Analysis Assistant")
